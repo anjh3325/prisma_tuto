f33ac250ce28ae41f2477a3726ed04ec096a2705
Anjh <anjh3325@gmail.com>
2023-09-12 13:10:38 +0900

    crud까지

--- src/todo/controller/todo.controller.ts
+++ src/todo/controller/todo.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TodoService } from '../service/todo.service';
 import { Todo } from '@prisma/client';
 
@@ -14,5 +23,28 @@ export class TodoController {
   @Get('/:id')
   async fetchTodoItem(@Param('id') id: number): Promise<Todo | null> {
     return this.todoService.fetchTodoItem(id);
+  }
+
+  @Delete('/:id')
+  async deleteTodoItem(@Param('id') id: number): Promise<Todo | null> {
+    return this.todoService.deleteTodoItem(id);
+  }
+
+  @Post()
+  async addTodoItem(@Body() data: Todo): Promise<Todo | null> {
+    return this.todoService.addTodoItem(data);
+  }
+
+  @Put('/:id')
+  async updateTodoItem(
+    @Param('id') id: number,
+    @Body() data: Todo,
+  ): Promise<Todo | null> {
+    return this.todoService.updateTodoItem(
+      id,
+      data.title,
+      data.content,
+      data.isDone,
+    );
   }
 }
--- src/todo/service/todo.service.ts
+++ src/todo/service/todo.service.ts
@@ -15,4 +15,27 @@ export class TodoService {
   async fetchTodoItem(id: number): Promise<Todo | null> {
     return this.prismaServie.todo.findUnique({ where: { id: Number(id) } });
   }
+
+  // 삭제
+  async deleteTodoItem(id: number): Promise<Todo | null> {
+    return this.prismaServie.todo.delete({ where: { id: Number(id) } });
+  }
+
+  // 추가
+  async addTodoItem(data: Todo): Promise<Todo | null> {
+    return this.prismaServie.todo.create({ data: data });
+  }
+
+  // 수정
+  async updateTodoItem(
+    id: number,
+    title: string,
+    content: string,
+    isDone: boolean,
+  ): Promise<Todo | null> {
+    return this.prismaServie.todo.update({
+      where: { id: Number(id) },
+      data: { title: title, content: content, isDone: isDone },
+    });
+  }
 }
